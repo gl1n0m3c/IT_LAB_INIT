36efdc29ecd40e1b0ffe9907b177f69716621c26
Nicolashka <nperewerzew2005@yandex.ru>
2024-03-05 23:37:32 +0300

    ADD tests for Decoder REFACTOR Decoder

--- internal/decoder/models.go
+++ internal/decoder/models.go
@@ -3,6 +3,7 @@ package decoder
 import (
 	"fmt"
 	"github.com/gl1n0m3c/IT_LAB_INIT/internal/models"
+	"strings"
 	"time"
 )
 
@@ -21,44 +22,49 @@ type CaseDataType1 struct {
 	Datetime         string `json:"datetime"`
 }
 
+type TransportInfo struct {
+	Chars   string `json:"chars"`
+	Numbers string `json:"numbers"`
+	Region  string `json:"region"`
+}
+
+type CameraInfo struct {
+	ID string `json:"id"`
+}
+
+type ViolationInfo struct {
+	ID    string `json:"id"`
+	Value string `json:"value"`
+}
+
+type SkillInfo struct {
+	Value int `json:"value"`
+}
+
+type DatetimeInfo struct {
+	Year      int    `json:"year"`
+	Month     int    `json:"month"`
+	Day       int    `json:"day"`
+	Hour      int    `json:"hour"`
+	Minute    int    `json:"minute"`
+	Seconds   int    `json:"seconds"`
+	UtcOffset string `json:"utc_offset"`
+}
+
 type CaseDataType2 struct {
-	Transport struct {
-		Chars   string `json:"chars"`
-		Numbers string `json:"numbers"`
-		Region  string `json:"region"`
-	} `json:"transport"`
-	Camera struct {
-		ID string `json:"id"`
-	} `json:"camera"`
-	Violation struct {
-		ID    string `json:"id"`
-		Value string `json:"value"`
-	} `json:"violation"`
-	Skill struct {
-		Value int `json:"value"`
-	} `json:"skill"`
-	Datetime struct {
-		Year      int    `json:"year"`
-		Month     int    `json:"month"`
-		Day       int    `json:"day"`
-		Hour      int    `json:"hour"`
-		Minute    int    `json:"minute"`
-		Seconds   int    `json:"seconds"`
-		UtcOffset string `json:"utc_offset"`
-	} `json:"datetime"`
+	Transport TransportInfo `json:"transport"`
+	Camera    CameraInfo    `json:"camera"`
+	Violation ViolationInfo `json:"violation"`
+	Skill     SkillInfo     `json:"skill"`
+	Datetime  DatetimeInfo  `json:"datetime"`
 }
 
 type CaseDataType3 struct {
-	Transport string `json:"transport"`
-	Camera    struct {
-		ID string `json:"id"`
-	} `json:"camera"`
-	Violation struct {
-		ID    string `json:"id"`
-		Value string `json:"value"`
-	} `json:"violation"`
-	Skill    int    `json:"skill"`
-	Datetime string `json:"datetime"`
+	Transport string        `json:"transport"`
+	Camera    CameraInfo    `json:"camera"`
+	Violation ViolationInfo `json:"violation"`
+	Skill     int           `json:"skill"`
+	Datetime  string        `json:"datetime"`
 }
 
 func (c CaseDataType1) CameraDataToCaseBase() (models.CaseBase, error) {
@@ -68,9 +74,11 @@ func (c CaseDataType1) CameraDataToCaseBase() (models.CaseBase, error) {
 	if err != nil {
 		return models.CaseBase{}, fmt.Errorf("Недопустимый тип времени")
 	}
+	parsedTime = parsedTime.UTC()
 
+	charsRunes := []rune(c.TransportChars)
 	caseData.CameraID = c.CameraID
-	caseData.Transport = c.TransportChars + c.TransportNumbers + c.TransportRegion
+	caseData.Transport = string(charsRunes[:1]) + c.TransportNumbers + string(charsRunes[1:]) + c.TransportRegion
 	caseData.ViolationID = c.ViolationID
 	caseData.ViolationValue = c.ViolationValue
 	caseData.Level = c.SkillValue
@@ -82,11 +90,50 @@ func (c CaseDataType1) CameraDataToCaseBase() (models.CaseBase, error) {
 func (c CaseDataType2) CameraDataToCaseBase() (models.CaseBase, error) {
 	var caseData models.CaseBase
 
+	c.Datetime.UtcOffset = strings.Replace(c.Datetime.UtcOffset, "+", "", 1)
+
+	datetime := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d+0%s:00",
+		c.Datetime.Year,
+		c.Datetime.Month,
+		c.Datetime.Day,
+		c.Datetime.Hour,
+		c.Datetime.Minute,
+		c.Datetime.Seconds,
+		c.Datetime.UtcOffset,
+	)
+
+	parsedTime, err := time.Parse(time.RFC3339, datetime)
+	if err != nil {
+		return models.CaseBase{}, fmt.Errorf("Недопустимый тип времени: %v", err)
+	}
+	parsedTime = parsedTime.UTC()
+
+	charsRunes := []rune(c.Transport.Chars)
+	caseData.CameraID = c.Camera.ID
+	caseData.Transport = string(charsRunes[:1]) + c.Transport.Numbers + string(charsRunes[1:]) + c.Transport.Region
+	caseData.ViolationID = c.Violation.ID
+	caseData.ViolationValue = c.Violation.Value
+	caseData.Level = c.Skill.Value
+	caseData.Datetime = parsedTime
+
 	return caseData, nil
 }
 
 func (c CaseDataType3) CameraDataToCaseBase() (models.CaseBase, error) {
 	var caseData models.CaseBase
+
+	parsedTime, err := time.Parse(time.RFC3339, c.Datetime)
+	if err != nil {
+		return models.CaseBase{}, fmt.Errorf("Недопустимый тип времени")
+	}
+	parsedTime = parsedTime.UTC()
+
+	caseData.CameraID = c.Camera.ID
+	caseData.Transport = c.Transport
+	caseData.ViolationID = c.Violation.ID
+	caseData.ViolationValue = c.Violation.Value
+	caseData.Level = c.Skill
+	caseData.Datetime = parsedTime
 
 	return caseData, nil
 }
--- internal/decoder/tests/decoder_test.go
+++ internal/decoder/tests/decoder_test.go
@@ -1,118 +1,135 @@
 package tests
 
-//import (
-//	"bytes"
-//	"encoding/binary"
-//	"fmt"
-//	"github.com/gl1n0m3c/IT_LAB_INIT/internal/decoder"
-//	leb1282 "github.com/jcalabro/leb128"
-//	"github.com/stretchr/testify/assert"
-//	"strings"
-//	"testing"
-//	"time"
-//)
-//
-//func EncodeString(buf *bytes.Buffer, key, value string) {
-//	keyBytes := []byte(key)
-//	valueBytes := []byte(value)
-//
-//	// Длина ключа (2 байта)
-//	binary.Write(buf, binary.BigEndian, uint16(len(keyBytes)))
-//	// Длина значения (2 байта)
-//	binary.Write(buf, binary.BigEndian, uint16(len(valueBytes)))
-//	// Тип значения (1 байт, 0x00 для строк)
-//	buf.WriteByte(0x00)
-//	// Ключ (UTF-8 строка в байтах)
-//	buf.Write(keyBytes)
-//	// Значение (UTF-8 строка в байтах)
-//	buf.Write(valueBytes)
-//}
-//
-//func EncodeInt(buf *bytes.Buffer, key string, value int) {
-//	keyBytes := []byte(key)
-//	valueBytes := leb1282.EncodeS64(int64(value))
-//
-//	// Длина ключа (2 байта)
-//	binary.Write(buf, binary.BigEndian, uint16(len(keyBytes)))
-//	// Длина значения (2 байта)
-//	binary.Write(buf, binary.BigEndian, uint16(len(valueBytes)))
-//	// Тип значения (1 байт, 0x01 для целых чисел)
-//	buf.WriteByte(0x01)
-//	// Ключ (UTF-8 строка в байтах)
-//	buf.Write(keyBytes)
-//	// Значение (UTF-8 строка в байтах)
-//	buf.Write(valueBytes)
-//}
-//
-//func EncodeTime(buf *bytes.Buffer, key string, value time.Time) {
-//	// Форматируем время в строку согласно указанному формату
-//	formattedTime := value.Format(time.RFC3339)
-//	formattedTime = strings.Replace(formattedTime, "Z", "+", 1)
-//	fmt.Printf("%#v\n", formattedTime)
-//
-//	// Преобразуем ключ в байты
-//	keyBytes := []byte(key)
-//	// Преобразуем отформатированное время в байты
-//	valueBytes := []byte(formattedTime)
-//
-//	// Длина ключа (2 байта)
-//	binary.Write(buf, binary.BigEndian, uint16(len(keyBytes)))
-//	// Длина значения (2 байта)
-//	binary.Write(buf, binary.BigEndian, uint16(len(valueBytes)))
-//	// Тип значения (1 байт, 0x00 для строк)
-//	buf.WriteByte(0x00)
-//	// Ключ
-//	buf.Write(keyBytes)
-//	// Значение
-//	buf.Write(valueBytes)
-//}
-//
-//func TestCameraType1(t *testing.T) {
-//	var buf, ans bytes.Buffer
-//
-//	now := time.Now()
-//	newNow := time.Date(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second(), 0, time.Local)
-//	camera := decoder.CaseDataType1{
-//		TransportChars:   "ABC",
-//		TransportNumbers: "1234",
-//		TransportRegion:  "77",
-//		CameraID:         "CAM123",
-//		ViolationID:      "VIO456",
-//		ViolationValue:   "Speeding",
-//		SkillValue:       5,
-//		Datetime:         newNow,
-//	}
-//
-//	fmt.Printf("%#v\n", camera.Datetime)
-//
-//	EncodeString(&buf, "transport_chars", camera.TransportChars)
-//	EncodeString(&buf, "transport_numbers", camera.TransportNumbers)
-//	EncodeString(&buf, "transport_region", camera.TransportRegion)
-//	EncodeString(&buf, "camera_id", camera.CameraID)
-//	EncodeString(&buf, "violation_id", camera.ViolationID)
-//	EncodeString(&buf, "violation_value", camera.ViolationValue)
-//	EncodeInt(&buf, "skill_value", camera.SkillValue)
-//	EncodeTime(&buf, "datetime", camera.Datetime)
-//
-//	binary.Write(&ans, binary.BigEndian, uint16(len(buf.Bytes())+2))
-//	ans.Write(buf.Bytes())
-//
-//	fmt.Println(len(ans.Bytes()), ans.Bytes())
-//
-//	res, err := decoder.Decoder(&ans)
-//	if err != nil {
-//		fmt.Println(err)
-//	} else {
-//		fmt.Printf("%#v\n", res)
-//	}
-//
-//	decodedCamera, err := MapToCameraType1(res)
-//	if err != nil {
-//		fmt.Println(err)
-//	}
-//
-//	assert.Equal(t, camera, decodedCamera)
-//	fmt.Println(camera.Datetime)
-//	fmt.Println(decodedCamera.Datetime)
-//
-//}
+import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
+	"github.com/gl1n0m3c/IT_LAB_INIT/internal/decoder"
+	leb1282 "github.com/jcalabro/leb128"
+	"github.com/stretchr/testify/assert"
+	"strconv"
+	"testing"
+)
+
+func EncodeString(buf *bytes.Buffer, key, value string) {
+	keyBytes := []byte(key)
+	valueBytes := []byte(value)
+
+	// Длина ключа (2 байта)
+	binary.Write(buf, binary.BigEndian, uint16(len(keyBytes)))
+	// Длина значения (2 байта)
+	binary.Write(buf, binary.BigEndian, uint16(len(valueBytes)))
+	// Тип значения (1 байт, 0x00 для строк)
+	buf.WriteByte(0x00)
+	// Ключ (UTF-8 строка в байтах)
+	buf.Write(keyBytes)
+	// Значение (UTF-8 строка в байтах)
+	buf.Write(valueBytes)
+}
+
+func EncodeInt(buf *bytes.Buffer, key string, value int) {
+	keyBytes := []byte(key)
+	valueBytes := leb1282.EncodeS64(int64(value))
+
+	// Длина ключа (2 байта)
+	binary.Write(buf, binary.BigEndian, uint16(len(keyBytes)))
+	// Длина значения (2 байта)
+	binary.Write(buf, binary.BigEndian, uint16(len(valueBytes)))
+	// Тип значения (1 байт, 0x01 для целых чисел)
+	buf.WriteByte(0x01)
+	// Ключ (UTF-8 строка в байтах)
+	buf.Write(keyBytes)
+	// Значение (UTF-8 строка в байтах)
+	buf.Write(valueBytes)
+}
+
+func TestCameraType1(t *testing.T) {
+	var buf, ans bytes.Buffer
+
+	EncodeString(&buf, "transport_chars", caseDataType1.TransportChars)
+	EncodeString(&buf, "transport_numbers", caseDataType1.TransportNumbers)
+	EncodeString(&buf, "transport_region", caseDataType1.TransportRegion)
+	EncodeString(&buf, "camera_id", caseDataType1.CameraID)
+	EncodeString(&buf, "violation_id", caseDataType1.ViolationID)
+	EncodeString(&buf, "violation_value", caseDataType1.ViolationValue)
+	EncodeInt(&buf, "skill_value", caseDataType1.SkillValue)
+	EncodeString(&buf, "datetime", caseDataType1.Datetime)
+
+	ans.Write(buf.Bytes())
+
+	res, err := decoder.Decoder(&ans)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("%#v\n", res)
+	}
+
+	decodedCamera, err := decoder.MapToStruct(res)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	resCase, err := decodedCamera.CameraDataToCaseBase()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, caseDataType1, decodedCamera)
+	assert.Equal(t, resultCase1, resCase)
+}
+
+func TestCameraType2(t *testing.T) {
+	var dataBytes []byte
+	for i := 0; i+8 <= len(byteString2); i += 8 {
+		byteString := byteString2[i : i+8]
+		byteValue, _ := strconv.ParseUint(byteString, 2, 8)
+		dataBytes = append(dataBytes, byte(byteValue))
+	}
+
+	result, err := decoder.Decoder(bytes.NewBuffer(dataBytes[2:]))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cameraModel, err := decoder.MapToStruct(result)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Log(cameraModel)
+
+	resCase, err := cameraModel.CameraDataToCaseBase()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, caseDataType2, cameraModel)
+	assert.Equal(t, resultCase2, resCase)
+}
+
+func TestCameraType3(t *testing.T) {
+	var dataBytes []byte
+	for i := 0; i+8 <= len(byteString3); i += 8 {
+		byteString := byteString3[i : i+8]
+		byteValue, _ := strconv.ParseUint(byteString, 2, 8)
+		dataBytes = append(dataBytes, byte(byteValue))
+	}
+
+	result, err := decoder.Decoder(bytes.NewBuffer(dataBytes[2:]))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cameraModel, err := decoder.MapToStruct(result)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	resCase, err := cameraModel.CameraDataToCaseBase()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, caseDataType3, cameraModel)
+	assert.Equal(t, resultCase3, resCase)
+}
